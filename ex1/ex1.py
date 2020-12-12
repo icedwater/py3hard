fb1fed2d5729f894b7d259240605274188489c7b
icedwater <icedwater@gmail.com>
2020-12-12 22:03:40 +0800

    Made script print only one line.

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -1,8 +1,1 @@
-print("Hello World!")
-print("Hello Again")
-print("I like typing this.")
 print("This is fun.")
-print('Yay! Printing.')
-print("I'd much rather you 'not'.")
-print('I "said" do not touch this.')
-print("one more line.")
